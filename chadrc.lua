6014696940d0d00935fded25f463872f14fd697d
Elliott Minns <elliott.minns@pm.me>
2023-12-22 13:21:58 -0600

    Initial commit

---
+++ chadrc.lua
@@ -0,0 +1,6 @@
+---@type ChadrcConfig
+local M = {}
+M.ui = {theme = 'catppuccin'}
+M.plugins = "custom.plugins"
+
+return M
---
+++ plugins.lua
@@ -0,0 +1,48 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+      }
+      return opts
+    end,
+  }
+}
+return plugins
